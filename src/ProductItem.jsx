6af1b2c3280321a3be8e94ccf0e15f2f447e0d0f
uzair_wafai <uzairwafai000@gmail.com>
2024-01-18 00:51:33 +0530

    column

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,5 +1,6 @@
 function ProductItem({ value }) {
   return (
+    <div className="grid-col-3">
       <div className="m-1">
         <img
           src="https://media.istockphoto.com/id/1396093398/photo/dish-meat-based.jpg?b=1&s=612x612&w=0&k=20&c=zZOE8hcW58pUaYugU1DfNR1J1oi8RD2qaMP3PD2Oj8w="
@@ -9,6 +10,7 @@ function ProductItem({ value }) {
         <h1>{value.name}</h1>
         <p>${value.price}</p>
         <p>{value.stock}</p>
+      </div>
       </div>
   );
 }
